5f207c9d57d916f102c9bd447a4698c19a7862b1
Alex Tran <alex.tran1502@gmail.com>
2022-09-15 12:35:08 -0500

    Add icon

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,7 +2,8 @@
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
-use tauri::{SystemTray, CustomMenuItem, SystemTrayMenu, SystemTrayMenuItem};
+
+use tauri::{api::cli::Matches, CustomMenuItem, SystemTray, SystemTrayMenu, SystemTrayMenuItem};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -14,22 +15,21 @@ fn main() {
     let system_tray = create_tray();
 
     tauri::Builder::default()
-    .system_tray(system_tray)
+        .system_tray(system_tray)
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
 
 fn create_tray() -> SystemTray {
-  let tray = SystemTray::new();
+    let tray = SystemTray::new();
 
-  let quit = CustomMenuItem::new("quit".to_string(), "Quit");
-  let hide = CustomMenuItem::new("hide".to_string(), "Hide");
-  let tray_menu = SystemTrayMenu::new()
-    .add_item(quit)
-    .add_native_item(SystemTrayMenuItem::Separator)
-    .add_item(hide);
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(quit)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(hide);
 
-  tray.with_menu(tray_menu)
-
+    tray.with_menu(tray_menu)
 }
--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,40 @@
 <script lang="ts">
+  import {
+    readBinaryFile,
+    writeTextFile,
+    readDir,
+    Dir,
+  } from "@tauri-apps/api/fs";
+  import { open, save } from "@tauri-apps/api/dialog";
+
+  const openFolderSelector = async () => {
+    const path = await open({
+      title: "Select a folder to upload",
+      defaultPath: null,
+      multiple: false,
+      directory: true,
+    });
+
+    if (path && typeof path == "string") {
+      const entries = await readDir(path, {
+        recursive: true,
+      });
+
+      for (const entry of entries) {
+        console.log("Top level entry", entry);
+        if (entry.children) {
+          console.log(entry.children);
+        }
+      }
+    }
+  };
 </script>
 
-<main class="container">
-  <h1 class="font-bold text-4xl">Welcome to Tauri!</h1>
+<main class="grid place-items-center place-content-center">
+  <h1>Immich Agent</h1>
+  <div>
+    <button class="border rounded-md px-4 py-2" on:click={openFolderSelector}
+      >Chose watch folder</button
+    >
+  </div>
 </main>
